0f66d5a3248118b77086d62da9e31d384533d90e
cihan <cbehen@gmail.com>
2019-06-24 16:52:03 +0100

    son style ve sprite degisiklik

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 //require('./gulp/tasks/styles'),
 require('./gulp/tasks/watch'),
-mixins = require('postcss-mixins'),
 require('./src/gulp/tasks/sprites'),
 require('gulp-rename');
 
